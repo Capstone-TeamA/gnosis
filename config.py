c6bb6a443024913e66fcd323fa08a784adc30477
lambdabyte <52540358+lambdabyte@users.noreply.github.com>
2020-02-09 12:02:35 -0500

    revision 1

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
